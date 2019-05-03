ad5e8ca4605573521416c87aa107f0e87c720e29
Bric Rogers <bricdrogers@gmail.com>
2019-05-02 22:36:41 -0500

    got typing working in editor

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 export const loop = function() {
-    console.log("running!!");
-}
+
+
+    var room:Room = Game.rooms['W35S7'];
+    var constructionSites:ConstructionSite[] = room.find(FIND_CONSTRUCTION_SITES);
+    var sources:Source[] = room.find(FIND_SOURCES)
+    var structures:Structure[] = room.find(FIND_STRUCTURES);
+    var spawns:Spawn[] = room.find(FIND_MY_SPAWNS);
+
+    console.log("constructionSites", constructionSites);
+    console.log("sources", sources);
+    console.log("structures", structures);
+    console.log("spawns", spawns);
+};
